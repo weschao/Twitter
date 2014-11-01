7eb90218d9847e1d008db29832ef09395fdc83c9
Wes Chao <weschao@fb.com>
2014-10-31 19:35:42 -0700

    Initial Commit

---
+++ Twitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Twitter
+//
+//  Created by Wes Chao on 10/31/14.
+//  Copyright (c) 2014 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Twitter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Twitter
+//
+//  Created by Wes Chao on 10/31/14.
+//  Copyright (c) 2014 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
